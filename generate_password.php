0793615b6089c5425047449d010b03bb122b22c8
Joan Sebastian Tamayo Rivera <pwnaoj@gmail.com>
2025-05-11 12:07:17 -0500

    refactor: Del script gen password

--- generate_password.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Use this script to generate password hashes
-$password = "123456";
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-echo "Original password: $password<br>";
-echo "Hashed password: $hashed_password";
-?>
